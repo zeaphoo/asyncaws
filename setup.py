704b4e0030ecd4b93c3de20f9435a476f83aa444
Zhuo Wei <zeaphoo@qq.com>
2020-02-22 21:44:58 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,43 @@ import re
 with io.open('asyncaws/__init__.py', 'rt', encoding='utf8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+
 setup(
-    name="asyncaws",
+    name='asyncaws',
     version=version,
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    description="asyncio aws sdk",
-    long_description="asyncio aws sdk",
-    url='http://github.com/zeaphoo/asyncaws/',
-    author="wei.zhuo",
-    author_email="zeaphoo@qq.com",
-    include_package_data=True,
+    url='https://github.com/zeaphoo/asyncaws/',
+    license='MIT',
+    author='Wei Zhuo',
+    author_email='zeaphoo@qq.com',
+    description='asyncio aws sdk.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['asyncaws'],
+    include_package_data=False,
     zip_safe=False,
-    license="MIT",
-    keywords=(),
-    platforms="any",
+    platforms='any',
     install_requires=['httpx'],
-    entry_points={
-    }
+    extras_require={
+        'dev': [
+            'pytest>=3',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.6',
+    entry_points='''
+    '''
 )
